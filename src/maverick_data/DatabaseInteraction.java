cc167f25ef5956dcd3f9e23f119e5d37503dffa1
Chris Vantine <justchris.vantine@gmail.com>
2018-07-08 14:50:56 -0400

    Updated DatabaseInteraction class documentation and use

--- src/maverick_data/DatabaseInteraction.java
+++ src/maverick_data/DatabaseInteraction.java
@@ -2,28 +2,62 @@ package maverick_data;
 
 import java.sql.*;
 
-
+/**
+ * A class for interacting with a MySQL database
+ *
+ * @author Joel Seidel
+ * @author Chris Vantine
+ */
 public class DatabaseInteraction {
 
-    Connection dbConn = createConnection();
-    public Connection createConnection(){
+    /**
+     * The connection to use when running queries
+     */
+    private Connection dbConn;
+
+    /**
+     * Create a new DatabaseInteraction object
+     *
+     * @param host the host to connect to
+     * @param port the port to connect on
+     * @param username the username to use
+     * @param password the password to use
+     */
+    public DatabaseInteraction(String host, int port, String username, String password) {
+        this.dbConn = this.createConnection(host, port, username, password);
+    }
+
+    /**
+     * createConnection establishes a connection to a MySQL DB with the given parameters
+     *
+     * @param host the host to connect to
+     * @param port the port to connect on
+     * @param username the username to use
+     * @param password the password to use
+     * @return the created Connection object
+     */
+    public Connection createConnection(String host, int port, String username, String password){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            Connection dbConn = DriverManager.getConnection("jdbc:mysql://staging-itemdb.mavericksystems.us:3306",
-                    "MavAdmin", "Lt7e^PV%6vRi5l4BcSw\\t");
-            return dbConn;
+            // build host string
+            String url = "jdbc:mysql://" + host + ":" + Integer.toString(port);
+            // create connection
+            return DriverManager.getConnection(url, username, password);
         }
         catch(ClassNotFoundException cnfE){
-            //Mysql driver is not present on the server (this shouldn't happen because it will be installed
+            // Mysql driver is not present on the server (this shouldn't happen because it will be installed
             cnfE.printStackTrace();
         }
         catch(SQLException sqlE) {
-            //Could not create connection
+            // Could not create connection
             System.out.println("Data connection failed");
         }
         return null;
     }
 
+    /**
+     * closeConnection closes the current DB connection
+     */
     public void closeConnection(){
         try{
             dbConn.close();
@@ -32,6 +66,27 @@ public class DatabaseInteraction {
         }
     }
 
+    /**
+     * prepareStatement prepares a String SQL statement for use with the connected DB
+     * @param sql the unprepared statement to parse
+     * @return the prepared statement
+     */
+    public PreparedStatement prepareStatement(String sql){
+        PreparedStatement preparedStatement;
+        try{
+            preparedStatement = dbConn.prepareStatement(sql);
+        } catch(SQLException sqlEx){
+            sqlEx.printStackTrace();
+            preparedStatement = null;
+        }
+        return preparedStatement;
+    }
+
+    /**
+     * query runs a PreparedStatement against the connected database
+     * @param queryStatement the statement to execute
+     * @return the results of the query
+     */
     public ResultSet query(PreparedStatement queryStatement){
         try{
             return queryStatement.executeQuery();
@@ -44,23 +99,16 @@ public class DatabaseInteraction {
         }
     }
 
+    /**
+     * nonQuery runs a non-query PreparedStatement against the connected database
+     * @param nonQueryStatement the statement to execute
+     */
     public void nonQuery(PreparedStatement nonQueryStatement){
         try{
             nonQueryStatement.executeUpdate();
         } catch(SQLException sqlException){
             System.out.println(sqlException.getMessage());
         }
-    }
-
-    public PreparedStatement prepareStatement(String sql){
-        PreparedStatement preparedStatement;
-        try{
-            preparedStatement = dbConn.prepareStatement(sql);
-        } catch(SQLException sqlEx){
-            sqlEx.printStackTrace();
-            preparedStatement = null;
-        }
-        return preparedStatement;
     }
 
 }
