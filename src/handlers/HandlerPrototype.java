913fede5fa28cb4d2c2fc55d5a977e0ef5118595
Joel Seidel <joeldseidel@gmail.com>
2018-09-14 02:49:51 -0400

    Verify token in handler super class

--- src/handlers/HandlerPrototype.java
+++ src/handlers/HandlerPrototype.java
@@ -1,5 +1,9 @@
 package handlers;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONObject;
 
@@ -29,6 +33,20 @@ public abstract class HandlerPrototype {
             System.out.println("Valid Request");
         } else {
             System.out.println("Invalid Request");
+        }
+    }
+
+    boolean verifyToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("localhost:6969")
+                    .build(); //Reusable verifier instance
+            DecodedJWT jwt = verifier.verify(token);
+            System.out.println("Token " + token + " was verified");
+            return true;
+        } catch (UnsupportedEncodingException useEx){
+            return false;
         }
     }
 
