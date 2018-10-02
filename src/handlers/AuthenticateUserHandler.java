ccf8b70754202977546233f9d32df3d3b49b9166
Joel Seidel <joeldseidel@gmail.com>
2018-10-02 16:18:18 -0400

    Adventures in documentation
    Added some documentation, took away some testing output lines

--- src/handlers/AuthenticateUserHandler.java
+++ src/handlers/AuthenticateUserHandler.java
@@ -19,30 +19,56 @@ import java.sql.SQLException;
 import com.auth0.jwt.algorithms.*;
 import com.auth0.jwt.*;
 
+/**
+ * @author Joel Seidel
+ *
+ * Determine the authenticity of a username and password pair and return user data
+ */
+
 public class AuthenticateUserHandler extends HandlerPrototype implements HttpHandler {
     private String response;
+    /**
+     * Constructor to set this handler's required keys on handler context creation
+     */
     public AuthenticateUserHandler(){
+        //Set required keys in array inherited from HandlerPrototype super class
         requiredKeys = new String[] {"username", "password", "returnUserData"};
     }
+    /**
+     * Entry point for handler. Get parameters, verify request validity, fulfill request, return response to client
+     * @param httpExchange inherited from super class, set from client with params
+     * @throws IOException thrown if there is an issue with writing response data to client
+     */
     public void handle(HttpExchange httpExchange) throws IOException {
-        System.out.println("Entered User Authentication Handler");
+        //Get parameters from client
         JSONObject requestParams = GetParameterObject(httpExchange);
+        //Determine validity of request parameters and validate token
         boolean isValidRequest = isRequestValid(requestParams);
+        //Display in server console validity of the request for testing purposes
         displayRequestValidity(isValidRequest);
         if(isValidRequest){
+            //Request was valid, fulfill the request with params
             fulfillRequest(requestParams);
         } else {
+            //Request was invalid, set response to reflect this
             this.response = "invalid request";
         }
+        //Create response to client
         int responseCode = isValidRequest ? 200 : 400;
         Headers headers = httpExchange.getResponseHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         httpExchange.sendResponseHeaders(responseCode, this.response.length());
-        System.out.println("Response to User Logon Request : " + this.response);
+        System.out.println("Response to Add Pallet Request : " + this.response);
+        //Write response to the client
         OutputStream os = httpExchange.getResponseBody();
         os.write(this.response.getBytes());
         os.close();
     }
+
+    /**
+     * Parse request parameters, query database to determine authenticity, format and structure user data, return requested data
+     * @param requestParams validated parameters sent by the client
+     */
     @Override
     protected void fulfillRequest(JSONObject requestParams){
         String username = requestParams.getString("username");
@@ -60,6 +86,12 @@ public class AuthenticateUserHandler extends HandlerPrototype implements HttpHan
         }
     }
 
+    /**
+     * Get user count from user database and determine authenticity
+     * @param username username parameter from client
+     * @param password password parameter from client
+     * @return boolean representing the authenticity of the user
+     */
     private boolean isUserValid(String username, String password){
         boolean userIsValid;
         boolean userExists = (UserDataManager.getUserCount(username) == 1);
@@ -75,6 +107,12 @@ public class AuthenticateUserHandler extends HandlerPrototype implements HttpHan
         return userIsValid;
     }
 
+    /**
+     * Get user data from database by username as key
+     * @param username key of username to query by
+     * @return JSONObject containing structured user data from database query
+     */
+    //Todo: Migrate this method functionality to use manager class
     private JSONObject getUserData(String username){
         System.out.println("Attempting to get user data for username : " + username);
         DatabaseInteraction database = new DatabaseInteraction(DatabaseType.AppData);
@@ -101,35 +139,39 @@ public class AuthenticateUserHandler extends HandlerPrototype implements HttpHan
 
     /**
      * Convert a result set into a JSON Array
-     * @param resultSet
-     * @return a JSONArray
-     * @throws Exception
+     * @param userDataResults result set from the database query for the user data
+     * @return a JSONArray containing the structured user data
+     * @throws Exception may be thrown in the structuring of the data and parsing from result set
      */
-    public static JSONArray getUserDataFormattedResponse(ResultSet userDataResults) throws Exception {
-        System.out.println("Getting NEW Formatted Response");
+    private JSONArray getUserDataFormattedResponse(ResultSet userDataResults) throws Exception {
         JSONArray jsonArray = new JSONArray();
+        //Loop through each of the user data results contained in the result set and parse to json object to be inserted into the array
         while (userDataResults.next()) {
+            //Get count of columns within result set
             int total_rows = userDataResults.getMetaData().getColumnCount();
+            //Loop through each of the columns to parse
             for (int i = 0; i < total_rows; i++) {
                 JSONObject obj = new JSONObject();
-                System.out.println("Got " + userDataResults.getObject(i + 1) + " for " + userDataResults.getMetaData().getColumnLabel(i + 1)
-                        .toLowerCase());
-                obj.put(userDataResults.getMetaData().getColumnLabel(i + 1)
-                        .toLowerCase(), userDataResults.getObject(i + 1));
+                System.out.println("Got " + userDataResults.getObject(i + 1) + " for " + userDataResults.getMetaData().getColumnLabel(i + 1).toLowerCase());
+                //Create json object from parsed data
+                obj.put(userDataResults.getMetaData().getColumnLabel(i + 1).toLowerCase(), userDataResults.getObject(i + 1));
+                //Put created json object into
                 jsonArray.put(obj);
             }
         }
         return jsonArray;
     }
 
-    //Create a JSON Web Token to pass to client on authenticated login
+    /**
+     * Create a JSON Web Token to pass to client on authenticated login
+     * @return string containing the generated token for the authenticated user session
+     */
     private static String createToken(){
         String token = "";
         try {
+            //Generate the user token
             Algorithm algorithm = Algorithm.HMAC256("secret");
-            token = JWT.create()
-            .withIssuer("localhost:6969")
-            .sign(algorithm);
+            token = JWT.create().withIssuer("localhost:6969").sign(algorithm);
         } catch (Exception exception){
             //Invalid Signing configuration / Couldn't convert Claims.
         }
--- src/handlers/EditItemHandler.java
+++ src/handlers/EditItemHandler.java
@@ -33,19 +33,26 @@ public class EditItemHandler extends HandlerPrototype implements HttpHandler {
     }
     public void handle(HttpExchange httpExchange) throws IOException {
         System.out.println("Enter item editing handler");
+        //Get parameters from client
         JSONObject requestParams = GetParameterObject(httpExchange);
+        //Determine validity of request parameters and validate token
         boolean isValidRequest = isRequestValid(requestParams);
+        //Display in server console validity of the request for testing purposes
         displayRequestValidity(isValidRequest);
-        if (isValidRequest) {
+        if(isValidRequest){
+            //Request was valid, fulfill the request with params
             fulfillRequest(requestParams);
         } else {
+            //Request was invalid, set response to reflect this
             this.response = "invalid request";
         }
+        //Create response to client
         int responseCode = isValidRequest ? 200 : 400;
         Headers headers = httpExchange.getResponseHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         httpExchange.sendResponseHeaders(responseCode, this.response.length());
-        System.out.println("Response to user data query : " + this.response);
+        System.out.println("Response to Add Pallet Request : " + this.response);
+        //Write response to the client
         OutputStream os = httpExchange.getResponseBody();
         os.write(this.response.getBytes());
         os.close();
@@ -65,40 +72,38 @@ public class EditItemHandler extends HandlerPrototype implements HttpHandler {
             this.response = responseObject.toString();
         }
         else{
-                //Perform requested operation
-                switch(field){
-                    case "name":
-                        if(newvalue.length() > 40 || newvalue.length() < 1){
-                            responseObject.put("message","NameLengthError");
-                            this.response = responseObject.toString();
-                        }
-                        else{
-                            ItemDataManager.editName(mid, newvalue);
-                            responseObject.put("message","Success");
-                            this.response = responseObject.toString();
-                        }
-                        break;
-                    case "category":
-                        if(newvalue.length() > 40 || newvalue.length() < 1){
-                            responseObject.put("message","CategoryLengthError");
-                            this.response = responseObject.toString();
-                        }
-                        else{
-                            ItemDataManager.editCategory(mid, newvalue);
-                            responseObject.put("message","Success");
-                            this.response = responseObject.toString();
-                        }
-                        break;
-                    case "delete":
-                        ItemDataManager.removeItem(mid);
+            //Perform requested operation
+            switch(field){
+                case "name":
+                    if(newvalue.length() > 40 || newvalue.length() < 1){
+                        responseObject.put("message","NameLengthError");
+                        this.response = responseObject.toString();
+                    } else{
+                        ItemDataManager.editName(mid, newvalue);
                         responseObject.put("message","Success");
                         this.response = responseObject.toString();
-                        break;
-                    default:
-                        responseObject.put("message","InvalidFieldError");
+                    }
+                    break;
+                case "category":
+                    if(newvalue.length() > 40 || newvalue.length() < 1){
+                        responseObject.put("message","CategoryLengthError");
                         this.response = responseObject.toString();
-                        break;
-                }
+                    } else {
+                        ItemDataManager.editCategory(mid, newvalue);
+                        responseObject.put("message","Success");
+                        this.response = responseObject.toString();
+                    }
+                    break;
+                case "delete":
+                    ItemDataManager.removeItem(mid);
+                    responseObject.put("message","Success");
+                    this.response = responseObject.toString();
+                    break;
+                default:
+                    responseObject.put("message","InvalidFieldError");
+                    this.response = responseObject.toString();
+                    break;
+            }
         }
     }
 }
