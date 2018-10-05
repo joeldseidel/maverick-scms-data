15c3aaea2da4ffebf2efdf204eb8ffa7a1cff145
Joel Seidel <joeldseidel@gmail.com>
2018-10-04 22:42:24 -0400

    Adventures in documentation (new)

--- src/handlers/BindPurchaseOrderLinePalletsHandler.java
+++ src/handlers/BindPurchaseOrderLinePalletsHandler.java
@@ -51,14 +51,23 @@ public class BindPurchaseOrderLinePalletsHandler extends HandlerPrototype implem
         os.write(this.response.getBytes());
         os.close();
     }
+
+    /**
+     * Parse the parameters, parse the pallet list, write each pallet binding to the database
+     * @param requestParams validated parameters from the client
+     */
     @Override
     protected void fulfillRequest(JSONObject requestParams){
+        //Parse the parameters from the request
         int poId = requestParams.getInt("poid");
         int poline = requestParams.getInt("poline");
         JSONArray palletsArray = requestParams.getJSONArray("pallets");
+        //Parse the json array to get maverick pallet items from data
         List<MaverickPallet> palletsToBind = PalletDataManager.parseFromJsonArray(palletsArray);
+        //Instantiate the data manager to interface with the pallet binding data
         PurchaseOrderDataManager purchaseOrderDataManager = new PurchaseOrderDataManager();
         for(MaverickPallet thisPallet : palletsToBind){
+            //For each pallet, write the pallet binding to the database
             purchaseOrderDataManager.bindPallet(poId, poline, thisPallet);
         }
     }
