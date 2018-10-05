737409ae13f8413ac4e84c30bd2561d97cbcfb95
Joel Seidel <joeldseidel@gmail.com>
2018-10-04 22:27:38 -0400

    Adventures in documentation Part 235834

--- src/handlers/RaisePalletMovementEventHandler.java
+++ src/handlers/RaisePalletMovementEventHandler.java
@@ -12,23 +12,39 @@ import java.io.OutputStream;
 
 public class RaisePalletMovementEventHandler extends HandlerPrototype implements HttpHandler {
     private String response;
+    /**
+     * Constructor for defining the required keys for the request parameters
+     */
     public RaisePalletMovementEventHandler(){
+        //Define the required keys in the super class
         requiredKeys = new String[] {"palletid", "type", "fromcid", "tocid", "token"};
     }
-    public void handle(HttpExchange httpExchange) throws IOException {
+    /**
+     * Entry point for handler. Get parameters, verify request validity, fulfill request, return response to client
+     * @param httpExchange inherited from super class, set from client with params
+     * @throws IOException thrown if there is an issue with writing response data to client
+     */
+    public void handle(com.sun.net.httpserver.HttpExchange httpExchange) throws IOException {
+        //Get parameters from client
         JSONObject requestParams = GetParameterObject(httpExchange);
+        //Determine validity of request parameters and validate token
         boolean isValidRequest = isRequestValid(requestParams);
+        //Display in server console validity of the request for testing purposes
         displayRequestValidity(isValidRequest);
-        if(isValidRequest){
+        if (isValidRequest) {
+            //Request was valid, fulfill the request with params
             fulfillRequest(requestParams);
         } else {
+            //Request was invalid, set response to reflect this
             this.response = "invalid request";
         }
+        //Create response to client
         int responseCode = isValidRequest ? 200 : 400;
         Headers headers = httpExchange.getResponseHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         httpExchange.sendResponseHeaders(responseCode, this.response.length());
-        System.out.println("Response to raise pallet movement event : " + this.response);
+        System.out.println("Response to Add Item Request : " + this.response);
+        //Write response to the client
         OutputStream os = httpExchange.getResponseBody();
         os.write(this.response.getBytes());
         os.close();
