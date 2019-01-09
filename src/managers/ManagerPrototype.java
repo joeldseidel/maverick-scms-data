593deb321918d3a76cd3d191ac31959e0feebf41
Joel Seidel <joeldseidel@gmail.com>
2019-01-09 17:37:58 -0500

    Manager prototype documentation

--- src/managers/ManagerPrototype.java
+++ src/managers/ManagerPrototype.java
@@ -3,13 +3,25 @@ package managers;
 import maverick_data.DatabaseInteraction;
 import maverick_types.DatabaseType;
 
+/**
+ * Prototype for manager objects to manage database connection open and closes
+ * @author Joel Seidel
+ */
 public abstract class ManagerPrototype {
     protected DatabaseInteraction database;
 
-    protected void initDb(DatabaseType dbType){
+    /**
+     * Create a new database interaction instance
+     * @param dbType database to connect to
+     */
+    void initDb(DatabaseType dbType){
         database = new DatabaseInteraction(dbType);
     }
 
+    /**
+     * Overridden finalize method to close the database connection
+     * It is here we pray for a speedy garbage collection process
+     */
     @Override
     protected void finalize(){
         //Try to finalize the super
